424b46e700aaf795fa91d23e82cd105d4055744e
Nate Smith <nates3233@gmail.com>
2025-12-09 19:18:43 -0700

    update

--- static/js/load_navbar.js
+++ static/js/load_navbar.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("navbar-container");
 
-  fetch("../../navbar.html")
+  // Detect GitHub Pages repo root
+  const repoName = window.location.pathname.split("/")[1];
+
+  // Build correct path
+  const basePath = window.location.hostname === "localhost"
+    ? "../../navbar.html"
+    : `/${repoName}/navbar.html`;
+
+  fetch(basePath)
     .then(response => response.text())
     .then(html => {
       container.innerHTML = html;
